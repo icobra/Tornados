1e6f8289ea16f91c540c8bf14aa5a000b11f3763
icobra <eiroglif@mail.ru>
2018-01-25 06:25:36 -0500

    Add test page

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 # coding: utf8
+# version 0.00001
 
 import tornado.web
 import tornado.httpserver
@@ -15,6 +16,7 @@ class Application(tornado.web.Application):
             (r"/", HomeHandler),
             (r"/about", AboutHandler),
             (r"/faq", FaqHandler),
+            (r"/test", TestHandler),
         ]
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
@@ -42,6 +44,14 @@ class AboutHandler(tornado.web.RequestHandler):
 class FaqHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Fast answers and questions")
+
+class TestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render(
+            "test.html",
+            page_title = "Test",
+            header_text = "Проверка функционала",
+        )
 
 def main():
     tornado.options.parse_command_line()
